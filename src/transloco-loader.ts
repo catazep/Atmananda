031b293d9420ddac1b4da74718e6b197e2f44ead
Georgescu Catalin <catazep@yahoo.com>
2026-05-07 14:32:39 +0300

    Simplify caching

--- src/transloco-loader.ts
+++ src/transloco-loader.ts
@@ -3,14 +3,14 @@ import { Translation, TranslocoLoader } from '@jsverse/transloco';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
 
-declare const BUILD_HASH: string;
+const CACHE_BUST = Math.random().toString(36).slice(2);
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
   private http = inject(HttpClient);
   public getTranslation(lang: string) {
     return this.http
-      .get<Translation>(`/assets/i18n/${lang}.json?v=${BUILD_HASH}`)
+      .get<Translation>(`/assets/i18n/${lang}.json?v=${CACHE_BUST}`)
       .pipe(map((t) => t));
   }
 }
